2be6029f45ac41458f7fa1c04453a2365f766fcd
brad.zdanivsky <brad.zdanivsky@dc971243-0a89-dcf2-b47a-c166fffa500b>
2012-04-27 06:44:57 +0000

    JUnit fixes

--- trunk/oculus/WEB-INF/src/test/SettingsTest.java
+++ trunk/oculus/WEB-INF/src/test/SettingsTest.java
@@ -14,16 +14,20 @@ import org.junit.Test;
 
 public class SettingsTest {
 
-	Settings settings = new Settings();
+	Settings settings = null;
 
 	@Before
-	public void setUp() throws Exception {
-		System.out.println("before..");
+	public void setUp() {
+		System.out.println(getClass().toString());
+		settings = new Settings();
+		if(Settings.settingsfile != null)
+			if(Settings.settingsfile.contains("null"))
+				fail("no settings file found");
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		System.out.println("after..");
+		System.out.println("tearDown(), after.. done");
 	}
 	
 	@Test
@@ -44,16 +48,45 @@ public class SettingsTest {
 			if (!defaults.containsKey(val))
 				fail("default setting missing: " + factory.toString());
 		}
+		
+		if(defaults.getProperty(FactorySettings.vlow.toString()).split("_").length != 4) 
+			 fail("vlow default values are invalid");
+		if(defaults.getProperty(FactorySettings.vmed.toString()).split("_").length != 4) 
+			 fail("vmed default values are invalid");
+		if(defaults.getProperty(FactorySettings.vhigh.toString()).split("_").length != 4) 
+			 fail("vhigh default values are invalid");
+		if(defaults.getProperty(FactorySettings.vfull.toString()).split("_").length != 4) 
+			 fail("vfull default values are invalid");
+		
+		if(settings.readSetting(FactorySettings.vlow.toString()).split("_").length != 4) 
+			 fail("vlow settings are invalid");
+		if(settings.readSetting(FactorySettings.vmed.toString()).split("_").length != 4) 
+			 fail("vmed settings are invalid");
+		if(settings.readSetting(FactorySettings.vhigh.toString()).split("_").length != 4) 
+			 fail("vhigh settings are invalid");
+		if(settings.readSetting(FactorySettings.vfull.toString()).split("_").length != 4) 
+			 fail("vfull settings are invalid");
+		
 	}
 
 	
 	@Test
 	public void validateOptionalSetting() {
+		
+		// test default example: 320_240_8_85
 		Properties defaults = OptionalSettings.createDeaults();
-		for (FactorySettings factory : FactorySettings.values()) {
+		String[] cords = defaults.getProperty(OptionalSettings.vself.toString()).split("_");
+		if(cords.length!=4) fail("vself options are invalid: " + cords.length); 
+		
+		// test in current file 
+		cords = settings.readSetting(OptionalSettings.vself.toString()).split("_");
+		if(cords.length!=4) fail("vself options are invalid in settings file"); 
+		
+		/*for (OptionalSettings factory : OptionalSettings.values()) {
 			String val = factory.toString();
 			if (!defaults.containsKey(val))
-				fail("default setting missing: " + factory.toString());
-		}
+				fail("optional setting missing: " + factory.toString());
+		}*/
+		
 	}
 }
